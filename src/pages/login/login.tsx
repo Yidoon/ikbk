e22ed3a90d7780b07d7eda6b5b20d2fc910c3e6f
Yidoon <1471797269@qq.com>
2019-11-07 22:28:27 +0800

    feat: 构建项目， 添加header栏

---
+++ src/pages/login/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './login.less';
+
+const Login: React.FC = () => {
+  return (
+    <div className="login">
+      Login
+    </div>
+  )
+}
+
+export default Login
