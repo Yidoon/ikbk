dfb096ad5bcce15ce2ee9b9f233a7d8107ff92dc
Yidoon <1471797269@qq.com>
2019-11-21 22:06:37 +0800

    update

--- src/components/leftmenu/leftmenu.tsx
+++ src/components/leftmenu/leftmenu.tsx
@@ -4,21 +4,39 @@ import './leftmenu.less';
 interface Props {
 
 }
+interface menuListType {
+  menu_name: string,
+  icon: string,
+  link_url: string,
+  [propName: string]: any
+}
 interface State {
-
+  menuList: menuListType[]
 }
 
 class LeftMenu extends React.Component<Props, State>{
   constructor (props: Props) {
     super(props);
+    this.state = {
+      menuList: [
+        {
+          menu_name: '我的列表',
+          icon: '',
+          link_url: ''
+        }
+      ]
+    }
   };
   render () {
+    let menuLit = this.state.menuList;
     return (
       <div className="left-menu">
         <div className="menu-wrap">
-          <a href="" className="menu-item">我的列表</a>
-          <a href="" className="menu-item">我的列表</a>
-          <a href="" className="menu-item">我的列表</a>
+        {menuLit.map((item) => {
+          return(
+          <a href="" className="menu-item">{item.menu_name}</a>
+          )
+        })}
         </div>
       </div>
     );
